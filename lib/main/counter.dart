c335864d62527135983a4ded0f1af99ed87e0fb6
bluemoon983 <msdrobot@naver.com>
2024-07-24 10:37:34 +0900

    New-project

---
+++ lib/main/counter.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Click Count'),
+              Text('$counter'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
